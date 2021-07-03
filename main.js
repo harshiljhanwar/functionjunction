71998e42825c440df9d329d7c6f6178548ec5e75
harshiljhanwar <jhhmysites@gmail.com>
2021-07-03 20:12:55 +0530

    initial message

---
+++ main.js
@@ -0,0 +1,6 @@
+var tags = document.getElementById("navigation__list");
+var check = document.getElementById("navi__toggle");
+
+tags.addEventListener("click", function() {
+    document.getElementById("navi__toggle").checked = false;
+})
